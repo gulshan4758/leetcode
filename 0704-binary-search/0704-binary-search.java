406181efe026c7599a6b384fd116462904dad50a
Gulshan kumar kushwaha <gk9548599@gmail.com>
2026-09-18 12:59:20 +0530

    Time: 0 ms (100%), Space: 48.3 MB (69.09%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int lp=0,rp=nums.length-1;
+      while(lp<=rp){
+            int mid=(lp+rp)/2;
+            if(nums[mid]==target){
+                return mid;
+                }
+            
+            else if(nums[mid]<target){
+                lp=mid+1;
+
+            }
+            else{
+                rp=mid-1;
+            }
+        }
+        return -1;
+    }
+}
